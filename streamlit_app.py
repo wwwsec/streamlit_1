60b487662ee4829b7c44afd694505a656f76cf45
wwwsec <wwwsec@users.noreply.github.com>
2024-05-04 22:51:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,36 +6,35 @@ content_B = "这是菜单项 B 的内容。"
 content_C = "这是菜单项 C 的内容。"
 content_D = "这是菜单项 D 的内容。"
 
-def showa():
-    st.write(content_A)
-    st.write(content_A)
-    st.write(content_A)
-    st.write(content_A)
-
-def showb():
-    st.write(content_B)
-
-def showc():
-    st.write(content_C)
-
-def showd():
-    st.write(content_D)
-
 # 定义方法来显示不同菜单项的内容
 def show_content(menu):
     if menu == 'A':
-        showa()
+        st.write(content_A)
     elif menu == 'B':
-        showb()
+        st.write(content_B)
     elif menu == 'C':
-        showc()
+        st.write(content_C)
     elif menu == 'D':
-        showd()
+        st.write(content_D)
 
-
-st.sidebar.title("xx")
-# 在左侧栏添加菜单项
-menu_selection = st.sidebar.radio("选择菜单项", ['A', 'B', 'C', 'D'])
+# 在左侧栏添加链接形式的菜单项
+menu_selection = st.sidebar.radio(
+    "",
+    [
+        '<a href="#A" style="text-decoration: none; color: inherit;">A</a>',
+        '<a href="#B" style="text-decoration: none; color: inherit;">B</a>',
+        '<a href="#C" style="text-decoration: none; color: inherit;">C</a>',
+        '<a href="#D" style="text-decoration: none; color: inherit;">D</a>'
+    ],
+    format_func=lambda x: ""
+)
 
 # 在右侧页面显示相应内容
-show_content(menu_selection)
+if menu_selection == '<a href="#A" style="text-decoration: none; color: inherit;">A</a>':
+    show_content('A')
+elif menu_selection == '<a href="#B" style="text-decoration: none; color: inherit;">B</a>':
+    show_content('B')
+elif menu_selection == '<a href="#C" style="text-decoration: none; color: inherit;">C</a>':
+    show_content('C')
+elif menu_selection == '<a href="#D" style="text-decoration: none; color: inherit;">D</a>':
+    show_content('D')
