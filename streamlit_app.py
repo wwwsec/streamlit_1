ddedb38d00eedeb00c7fbad8b8b0a8ce8da2bbef
wwwsec <wwwsec@users.noreply.github.com>
2024-05-04 16:00:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ st.write('Contents of the `.streamlit/config.toml` file of this app')
 
 st.code("""
 [theme]
-primaryColor="#F39C12"
-backgroundColor="#2E86C1"
+primaryColor="white"
+backgroundColor="red"
 secondaryBackgroundColor="#AED6F1"
 textColor="#FFFFFF"
 font="monospace"
