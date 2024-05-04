6ebc0424901d0a05cfbc0617b64e4cf757a2f988
wwwsec <wwwsec@users.noreply.github.com>
2024-05-04 22:33:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,16 +6,30 @@ content_B = "这是菜单项 B 的内容。"
 content_C = "这是菜单项 C 的内容。"
 content_D = "这是菜单项 D 的内容。"
 
+def showa():
+    st.write(content_A)
+
+def showb():
+    st.write(content_B)
+
+def showc():
+    st.write(content_C)
+
+def showd():
+    st.write(content_D)
+
 # 定义方法来显示不同菜单项的内容
 def show_content(menu):
     if menu == 'A':
-        st.write(content_A)
+        showa()
     elif menu == 'B':
-        st.write(content_B)
+        showb()
     elif menu == 'C':
-        st.write(content_C)
+        showc()
     elif menu == 'D':
-        st.write(content_D)
+        showd()
+
+
 
 # 在左侧栏添加菜单项
 menu_selection = st.sidebar.radio("选择菜单项", ['A', 'B', 'C', 'D'])
