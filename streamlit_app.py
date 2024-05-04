aa1a6571b3baffad3e8a2a20235b2eab206b44f3
wwwsec <wwwsec@users.noreply.github.com>
2024-05-04 16:11:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,36 @@
 import streamlit as st
-import pandas as pd
 
-st.title('st.file_uploader')
+st.set_page_config(layout="wide")
 
-st.subheader('Input CSV')
-uploaded_file = st.file_uploader("Choose a file")
+st.title('How to layout your Streamlit app')
 
-if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
-  st.subheader('DataFrame')
-  st.write(df)
-  st.subheader('Descriptive Statistics')
-  st.write(df.describe())
-else:
-  st.info('☝️ Upload a CSV file')
+with st.expander('About this app'):
+  st.write('This app shows the various ways on how you can layout your Streamlit app.')
+  st.image('https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png', width=250)
+
+st.sidebar.header('Input')
+user_name = st.sidebar.text_input('What is your name?')
+user_emoji = st.sidebar.selectbox('Choose an emoji', ['', '😄', '😆', '😊', '😍', '😴', '😕', '😱'])
+user_food = st.sidebar.selectbox('What is your favorite food?', ['', 'Tom Yum Kung', 'Burrito', 'Lasagna', 'Hamburger', 'Pizza'])
+
+st.header('Output')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+  if user_name != '':
+    st.write(f'👋 Hello {user_name}!')
+  else:
+    st.write('👈  Please enter your **name**!')
+
+with col2:
+  if user_emoji != '':
+    st.write(f'{user_emoji} is your favorite **emoji**!')
+  else:
+    st.write('👈 Please choose an **emoji**!')
+
+with col3:
+  if user_food != '':
+    st.write(f'🍴 **{user_food}** is your favorite **food**!')
+  else:
+    st.write('👈 Please choose your favorite **food**!')
