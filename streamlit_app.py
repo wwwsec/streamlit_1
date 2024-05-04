3e83de7455e8e9174fea338a46fcfc0858609662
wwwsec <wwwsec@users.noreply.github.com>
2024-05-04 15:56:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,9 @@
 import streamlit as st
-import pandas as pd
-import pandas_profiling
-from streamlit_pandas_profiling import st_profile_report
 
-st.header('`streamlit_pandas_profiling`')
+st.header('st.latex')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-
-pr = df.profile_report()
-st_profile_report(pr)
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
